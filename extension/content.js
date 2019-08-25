30de5a083d448c18721227ed5b09359dbe8254e3
Damien Erambert <damien.erambert@me.com>
2019-08-25 05:21:26 -0700

    support Explore page as well

--- extension/content.js
+++ extension/content.js
@@ -1,15 +1,44 @@
 function switchAttribute(el, attr) {
   const currAttr = el.getAttribute(attr) || '';
-  if (!currAttr) return;
+  if (!currAttr) {
+    return;
+  }
 
   el.setAttribute(attr, currAttr.replace('.gifv', '.gif'));
 }
 
+function switchLightbox(el) {
+  const lightboxJSONString = el.getAttribute('data-lightbox');
+
+  if (!lightboxJSONString) {
+    return;
+  }
+
+  const lightboxJSON = JSON.parse(lightboxJSONString);
+
+  for (const key in lightboxJSON) {
+    if (lightboxJSON.hasOwnProperty(key)) {
+      const element = lightboxJSON[key];
+      if (String(element).endsWith('.gifv')) {
+        lightboxJSON[key] = String(element).replace('.gifv', '.gif');
+      }
+    }
+  }
+
+  el.setAttribute('data-lightbox', JSON.stringify(lightboxJSON));
+}
+
 const posts = document.querySelector('#posts');
+console.log('posts', posts);
 
 const onMutation = () => {
+  if (!posts) {
+    return;
+  }
+
   const GIFsLinks = posts.querySelectorAll(`[href*=".gifv"]`);
   const GIFSImages = posts.querySelectorAll(`[src*=".gifv"]`);
+  const GIFSDataSrc = posts.querySelectorAll(`[data-src*=".gifv"]`);
 
   Array.from(GIFsLinks).forEach(el => {
     switchAttribute(el, 'href');
@@ -18,14 +47,27 @@ const onMutation = () => {
   Array.from(GIFSImages).forEach(el => {
     switchAttribute(el, 'src');
   });
+  Array.from(GIFSDataSrc).forEach(el => {
+    switchAttribute(el, 'data-src');
+  });
 
-  console.log(`Switched ${GIFsLinks.length} links and ${GIFSImages.length} images to GIFs`);
+  const Slideshows = posts.querySelectorAll('[data-lightbox]');
+
+  Array.from(Slideshows).forEach(el => switchLightbox(el));
+
+  console.log({
+    GIFsLinks: GIFsLinks.length,
+    GIFSImages: GIFSImages.length,
+    GIFSDataSrc: GIFSDataSrc.length,
+    Slideshows: Slideshows.length
+  });
 };
 
 const postsObserver = new MutationObserver(onMutation);
 
 postsObserver.observe(posts, {
-  childList: true
+  childList: true,
+  subtree: true
 });
 
 onMutation();
